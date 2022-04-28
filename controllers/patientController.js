b35af0d571c90d9b880cfa7f2318c9f1fa2bcff5
czx20010217 <82380384+czx20010217@users.noreply.github.com>
2022-04-28 19:32:57 +1000

    add constraint to one patient can only submit one record perday

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -2,10 +2,19 @@ const Patient = require('../models/patient')
 const Record = require('../models/record')
 
 // add records
-const insertRecord = (req,res) => {
+const insertRecord = async (req,res) => {
     // hard code user for temporary usage
-    console.log("adding new record, input param: ", req.body)
-
+    patient_id = "625bd3b2263cf5c4c2442a16"
+    // get current date
+    var now = new Date()
+    const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    // console.log(startOfToday)
+    // console.log("adding new record, input param: ", req.body)
+    const record = await Record.findOne({patient_id: patient_id, updatedAt: {$gte: startOfToday}}).lean()
+    if (record){
+        res.render("recordSubmited")
+        return
+    }
     const {
         blood_glucose_level, blood_glucose_level_commment, 
         weight, weight_commment, 
@@ -14,7 +23,7 @@ const insertRecord = (req,res) => {
 
     patient_id = "625bd3b2263cf5c4c2442a16"
     submitted = true
-    const newRecord = new Record({  patient_id, submitted, 
+    const newRecord = new Record({ patient_id, submitted, 
         blood_glucose_level, blood_glucose_level_commment, 
         weight, weight_commment, 
         doses_of_insulin_taken, doses_of_insulin_taken_commment, 
@@ -26,6 +35,7 @@ const insertRecord = (req,res) => {
 }
 
 const getDashBoard = (req,res) => {
+
     try { 
         return res.render('patientDashBoard')
     } catch (err) { 
@@ -33,7 +43,17 @@ const getDashBoard = (req,res) => {
     } 
 }
 
-const addDailyRecord = (req,res) => {
+const addDailyRecord = async (req,res) => {
+    // hard code patient id
+    patient_id = "625bd3b2263cf5c4c2442a16"
+    var now = new Date()
+    const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+
+    const record = await Record.findOne({patient_id: patient_id, updatedAt: {$gte: startOfToday}}).lean()
+    if (record){
+        res.render("recordSubmited")
+        return
+    }
     try { 
         return res.render('addRecords')
     } catch (err) { 
